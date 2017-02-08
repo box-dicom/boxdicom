acce8217d44405da93698adec2da3b7ef9678a96
Cody Ebberson <cody@ebberson.com>
2017-02-08 13:52:10 -0800

    Proxy version 2.6.0

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-aws s3 sync ./ s3://boxdicom.com/ --profile box --region us-west-1 --acl public-read --recursive --exclude ".git/*" --exclude ".DS_Store"
+aws s3 sync ./ s3://boxdicom.com/ --profile box --region us-west-1 --acl public-read --size-only --exclude ".git/*" --exclude ".DS_Store"
 
